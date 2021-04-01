377e88436f37c64c99413837518f467719fff534
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2021-04-01 15:26:52 +0300

    add Validations
    validate date and input

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ function cleanInput() {
 /**
  * Global Vars to get elements
 */
-const dataBox = document.getElementById("dateInput")
-const houtBox = document.getElementById("hourInput")
+const textBox = document.getElementById("input-text")
+const dataBox = document.getElementById("date-input")
+const timeBox = document.getElementById("hour-input")
 
 
 /**
@@ -44,14 +45,14 @@ function saveNoteToLocalStorage(text, date, time) {
     }
 
     let noteList = [];
-    const jsonArray = localStorage.getItem("notes");
+    const jsonArray = localStorage.getItem("notes")
     if (jsonArray) {
-        noteList = JSON.parse(jsonArray);
+        noteList = JSON.parse(jsonArray)
     }
 
-    noteList.push(note);
-    localStorage.setItem("noteList", JSON.stringify(noteList));
-    return note;
+    noteList.push(note)
+    localStorage.setItem("noteList", JSON.stringify(noteList))
+    return note
 }
 
 /**
@@ -76,5 +77,45 @@ function addTask() {
     const span1 = document.getElementById("tasks")
     img.src = "/assets/images/notebg.png"
     span1.appendChild(img)
+}
+
+/**
+ * Note Validator (text,date,time)
+ * Validate that the text is note empty, or it's contains white spaces in the beginning 
+ * date is note empty or it's not in the past
+ * time is note in the past
+ * @param text string
+ * @param date date
+ * @param time time
+*/
+function validation(text, date, time) {
+    const currentDate = new Date()
+    const parsedDate = date.split("-")
+    const currentDateStruct={
+        year: parseInt(currentDate.getFullYear()),
+        month: parseInt(currentDate.getMonth() + 1),
+        day: parseInt(currentDate.getDate())
+    }
+    const inputDateStruct={
+        year: parseInt(parsedDate[0]),
+        month: parseInt(parsedDate[1]),
+        day: parseInt(parsedDate[2])        
+    }
+    if (text === "") {
+        alert("text cannot be empty\nPlease enter some text in the text box")
+        return;
+    }
+    if (date === ""){
+        alert("date cannot be empty\nif you want to add note you must choose date")
+    }
+
+
+}
+function addTaskToBoard() {
+    const noteText = textBox.value
+    const noteDate = dataBox.value
+    const noteTime = timeBox.value
+
+    validation(noteText, noteDate, noteTime)
 
 }
