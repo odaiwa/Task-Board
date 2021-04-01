63d5a61729b1692fee938960cec7d2370c61c99d
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2021-04-01 20:19:59 +0300

    Final Commit
    Fix all bugs

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function cleanInput() {
     document.getElementById("date-input").value = ""
     document.getElementById("hour-input").value = ""
 }
+
+console.log(`All the prints that will appear on the console is for debugging matter.`)
 /**
  * Global Vars to get elements from DOM
  */
@@ -13,7 +15,8 @@ const textBox = document.getElementById("input-text")
 const dataBox = document.getElementById("date-input")
 const timeBox = document.getElementById("hour-input")
 const tasksBoard = document.getElementById("tasks-board")
-
+let toBeDeleted
+let functioncaller = 0
 /**
  * Clean all date from LocalStorage
  * also calls cleanInput()
@@ -24,12 +27,14 @@ function cleanStorage() {
     document.getElementById("tasks-board").innerHTML = ""
 }
 
-function deleteNoteFromLocalStorage(nid) {
+function deleteNoteFromLocalStorage(removeNID) {
+    console.log(removeNID+ "delete note from local")
+    console.log(`Task with id = ${removeNID} is in removing process`)
     const jsonArray = localStorage.getItem("noteList");
     //if (!jsonArray) return;
     const noteList = JSON.parse(jsonArray);
     for (let i = 0; i < noteList.length; i++) {
-        if (noteList[i].nid === nid) {
+        if (noteList[i].nid === removeNID) {
             const index = noteList.indexOf(noteList[i]);
             if (index > -1) {
                 noteList.splice(index, 1);
@@ -40,7 +45,14 @@ function deleteNoteFromLocalStorage(nid) {
     localStorage.setItem("noteList", JSON.stringify(noteList));
 }
 
-
+function deleteTask(task){
+    console.log(`delete note has been called with id ${task.id}`)
+    deleteNoteFromLocalStorage(task.id);
+    task.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        task.remove()
+    }, 200);
+}
 /**
  * Save note to LocalStorage
  * Makes a uniq id for each note add this id to note struct, and then we add text of the note date of the note and time of the note
@@ -51,8 +63,8 @@ function deleteNoteFromLocalStorage(nid) {
  */
 function saveNoteToLocalStorage(text, date, time) {
     console.log("saving to localStorage started")
-        //UNIQ id for each Note.
-        //making uniq id for removal usage
+    //UNIQ id for each Note.
+    //making uniq id for removal usage
     const nId = uniqIdForNote();
     //note Struct
     const note = {
@@ -101,7 +113,7 @@ function uniqIdForNote() {
  * @param date date
  * @param time time
  */
-function validation(text, date, time, from) {
+function validation(text, date, time) {
     const currentDate = new Date()
     const parsedDate = date.split("-")
     const currentHour = currentDate.getHours();
@@ -155,10 +167,65 @@ function validation(text, date, time, from) {
             }
         }
     }
-    console.log("Validation succeeded ", from)
+    console.log("Validation succeeded ")
     return true;
 }
+function validateTimeForOnLoad(time) {
+    if(time === ""){
+        return true
+    }
+    const currentDate = new Date()
+    console.log("deleting duo to invalid time")
+    const currentHour = currentDate.getHours()
+    const currentMinutes = currentDate.getMinutes()
 
+    const [hour, minutes] = time.split(":");
+    const [intHour, intMinutes] = [parseInt(hour), parseInt(minutes)]
+
+    if (currentHour > intHour) {
+        alert(`invalid hour\nhour cannot be in past\n`)
+        return false;
+    } else if (currentHour == intHour) {
+        if (currentMinutes > intMinutes) {
+            alert("invalid minute \nminute cannot be in past")
+            return false;
+        }
+    }
+    return true
+
+}
+function validateHourForOnLoad(date) {
+    const currentDate = new Date()
+    const parsedDate = date.split("-")
+    const currentDateStruct = {
+        year: parseInt(currentDate.getFullYear()),
+        month: parseInt(currentDate.getMonth() + 1),
+        day: parseInt(currentDate.getDate())
+    }
+    const inputDateStruct = {
+        year: parseInt(parsedDate[0]),
+        month: parseInt(parsedDate[1]),
+        day: parseInt(parsedDate[2])
+    }
+    if (inputDateStruct.year < currentDateStruct.year) {
+        console.log("rejected from hour validator")
+        return false
+    }
+    else if (inputDateStruct.year === currentDateStruct.year) {
+        if (inputDateStruct.month < currentDateStruct.month) {
+            console.log("rejected from hour validator")
+            return false
+        }
+        else if (inputDateStruct.month === currentDateStruct.month) {
+            if (inputDateStruct.day < currentDateStruct.day) {
+                console.log("rejected from hour validator")
+
+                return false
+            }
+        }
+    }
+    return true
+}
 function addTaskToBoard() {
     const noteText = textBox.value
     const noteDate = dataBox.value
@@ -179,12 +246,11 @@ function showTaskonBoard(task) {
     const taskDiv = document.createElement("Div")
     taskDiv.className = "task"
     taskDiv.id = task.nid
-
     const glyphDeleteIcon = document.createElement("i")
-    glyphDeleteIcon.classList.add("glyphicon", "glyphicon-remove-circle")
-    glyphDeleteIcon.setAttribute("onclick", `deleteNoteFromLocalStorage(${task.nid})`)
+    glyphDeleteIcon.classList.add("far","fa-window-close")
+    glyphDeleteIcon.setAttribute("onclick", `deleteTask(${task.nid})`)
     taskDiv.appendChild(glyphDeleteIcon)
-
+    
     const taskText = document.createElement("Div")
     taskText.className = ("task-text")
     task.text = task.text.replaceAll("\n", "<br>")
@@ -203,22 +269,22 @@ function onload() {
     const jsonArray = localStorage.getItem("noteList")
     if (!jsonArray) return
     const notes = JSON.parse(jsonArray)
-    let notesBoardContent = ""
-    for (const note of notes) {
-        notesBoardContent += creatNoteElement(note)
+    let taskOnBoard = ""
+    for (const task of notes) {
+        taskOnBoard += creatNoteElement(task)
     }
-    const notesBoard = document.getElementById("tasks-board")
-    notesBoard.innerHTML += notesBoardContent
+    tasksBoard.innerHTML += taskOnBoard
 }
 
 
 function creatNoteElement(task) {
-    //if the task is from yesterday
-    if (validation(task.text, task.date, task.time, "from create note element")) {
+    const taskID = task.nid
+    if (validateHourForOnLoad(task.date) && validateTimeForOnLoad(task.time)) {
         task.text = task.text.replaceAll("\n", "<br>"); //FOR ADDING NEW LINE
+        console.log(taskID+"from create ")
         return `
-            <div class="note" id="${task.nid}">
-                <i class="glyphicon glyphicon-remove-circle" onclick="deleteNoteFromLocalStorage(${task.nid})" ></i>
+            <div class="task" id="${task.nid}">
+                <i class="far fa-window-close" onclick="deleteTask(${taskID})" ></i>
                 <div class="task-text">
                     ${task.text}
                 </div>
@@ -227,10 +293,11 @@ function creatNoteElement(task) {
                     <br>
                     ${task.time}    
                 </div>
-            </div>`;
-    } else {
-        deleteFromLocalStorage(task.nid);
-        return "";
+            </div>`
+    } 
+    else {
+        deleteNoteFromLocalStorage(taskID)
+        return ""
     }
 }
 onload()
