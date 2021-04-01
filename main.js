f1ae91ee79f212fe5c67b7afb42e1e9d3234d115
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2021-04-01 14:39:03 +0300

    add note to localStorage

--- main.js
+++ main.js
@@ -1,98 +1,80 @@
+/**
+ * Clean Form from input
+*/
 function cleanInput() {
     document.getElementById("input-text").value = ""
     document.getElementById("date-input").value = ""
     document.getElementById("hour-input").value = ""
 }
-
+/**
+ * Global Vars to get elements
+*/
 const dataBox = document.getElementById("dateInput")
 const houtBox = document.getElementById("hourInput")
-//const addTask = document.getElementById("addTask")
 
-/*
-inputBox.onkeyup = () => {
-    let userEnteredValue = inputBox.value; //getting user entered value
-    if (userEnteredValue.trim() != 0) { //if the user value isn't only spaces
-        addBtn.classList.add("active"); //active the add button
-    } else {
-        addBtn.classList.remove("active"); //unactive the add button
-    }
-}
+
+/**
+ * Clean all date from LocalStorage
 */
-
-function cleanStorage(){
+function cleanLocalStorage() {
+    cleanInput();
     localStorage.clear();
 }
 
-function addToLocalStorage(){
-    const dataBox = document.getElementById("dateInput")
-    const houtBox = document.getElementById("hourInput")
-
-}
 
 
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-  
-
-//showTasks();
-
-
-function saveToLocalStorage(noteText, noteDate, noteTime) {
-
-    // giving each note a unique id so it can be used later,e.g.:removal of the note. 
-    const noteId = createId();
-
+/**
+ * Save note to LocalStorage
+ * Makes a uniq id for each note add this id to note struct, and then we add text of the note date of the note and time of the note
+ * then we add it to the LocalStorage.
+ * @param string text - inner value of the note
+ * @param date date - expiration date of the note
+ * @param time time - (optional) expiration hour of the Task
+ */
+function saveNoteToLocalStorage(text, date, time) {
+    //UNIQ id for each Note.
+    //making uniq id for removal usage
+    const nId = uniqIdForNote();
+    //note Struct
     const note = {
-        nid: noteId,
-        body: noteText,
-        date: noteDate,
-        time: noteTime
+        nid: nId,
+        body: text,
+        date: date,
+        time: time
     }
 
-    let notes = [];
+    let noteList = [];
     const jsonArray = localStorage.getItem("notes");
     if (jsonArray) {
-        notes = JSON.parse(jsonArray);
+        noteList = JSON.parse(jsonArray);
     }
 
-    notes.push(note);
-    localStorage.setItem("notes", JSON.stringify(notes));
+    noteList.push(note);
+    localStorage.setItem("noteList", JSON.stringify(noteList));
     return note;
-
 }
 
-/*
-function showTasks() {
-    let getLocalStorageData = localStorage.getItem("New Todo");
-    if (getLocalStorageData == null) {
-        listArray = [];
-    } else {
-        listArray = JSON.parse(getLocalStorageData);
-    }
-    const pendingTasksNumb = document.querySelector(".pendingTasks");
-    pendingTasksNumb.textContent = listArray.length; //passing the array length in pendingtask
-    if (listArray.length > 0) { //if array length is greater than 0
-        deleteAllBtn.classList.add("active"); //active the delete button
-    } else {
-        deleteAllBtn.classList.remove("active"); //unactive the delete button
-    }
-    let newLiTag = "";
-    listArray.forEach((element, index) => {
-        newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-    });
-    todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
-    inputBox.value = ""; //once task added leave the input field blank
-}
+/**
+ * Create UNIQ id for note
+ * @param NONE
 */
+function uniqIdForNote() {
+    const idString = String.fromCharCode(Math.floor((Math.random() * 25) + 65));
+    do {
+        const asciiCode = Math.floor((Math.random() * 42) + 48);
+        if (asciiCode < 58 || asciiCode > 64) {
+            idString += String.fromCharCode(asciiCode);
+        }
+    } while (idString.length < 32);
 
-function addTask(){
+    return idString;
+}
+
+
+function addTask() {
     const img = document.createElement("img")
     const span1 = document.getElementById("tasks")
-    img.src="/assets/images/notebg.png"
+    img.src = "/assets/images/notebg.png"
     span1.appendChild(img)
 
 }
